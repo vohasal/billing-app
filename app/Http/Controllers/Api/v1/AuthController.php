ce902c1e6ff2f62d7e110e3b92bba0b0c82ba07a
vohasal <kulyashovr@gmail.com>
2026-01-30 18:06:31 +0500

    feat(api): add login, register

--- app/Http/Controllers/Api/v1/AuthController.php
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -3,9 +3,36 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function register(StoreUserRequest $request)
+    {
+        return User::query()->create($request->validated());
+    }
+
+    public function login(LoginUserRequest $request)
+    {
+        if (!Auth::attempt($request->only(['email', 'password']))){
+            return response()->json([
+                'message' => 'Wrong email or password'
+            ], 401);
+        }
+
+        $user = Auth::user();
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken("Token of user: {$user->name}")->plainTextToken
+        ]);
+    }
+
+    public function logout()
+    {
+
+    }
 }
