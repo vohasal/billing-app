50fdbe4b180e51bf949ae4a222238d943612d296
vohasal <kulyashovr@gmail.com>
2026-01-30 14:47:46 +0500

    feat(api): add rules

--- app/Http/Requests/StoreCurrencyRequest.php
+++ app/Http/Requests/StoreCurrencyRequest.php
@@ -11,7 +11,7 @@ class StoreCurrencyRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class StoreCurrencyRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:125'],
+            'code' => ['required', 'string', 'max:5']
         ];
     }
 }
