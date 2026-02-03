f9d917146a626c002dc79e438755c6537b6ac6ee
vohasal <kulyashovr@gmail.com>
2026-02-03 17:31:20 +0500

    fix(provider): rate limiter is not working for get methods

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,8 +22,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        RateLimiter::for('api', function (Request $request){
-            return Limit::perMinute(5)->by($request->user()?->id ?: $request->ip());
+        RateLimiter::for('api', function (Request $request) {
+            if ($request->isMethod('get')) {
+                return Limit::none();
+            }
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
     }
 }
