7853c941011bf39757ad00b76357db9ec16b882e
vohasal <kulyashovr@gmail.com>
2026-02-03 16:22:00 +0500

    feat(api): add  filter

--- app/Http/Controllers/Api/v1/TransactionController.php
+++ app/Http/Controllers/Api/v1/TransactionController.php
@@ -7,17 +7,51 @@ use App\Models\Transaction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTransactionRequest;
 use App\Http\Requests\UpdateTransactionRequest;
+use Auth;
+use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return Transaction::orderBy('date', 'desc')
-            ->orderBy('id', 'desc')
-            ->cursorPaginate(5);
+
+        $query = Transaction::with(['category', 'account']);
+
+        $query->whereHas('account', function ($q){
+            $q->where('user_id', Auth::id());
+        });
+
+        $query->when($request->date_from, function ($q, $dateFrom) {
+            $q->where('date', '>=', $dateFrom);
+        });
+
+        $query->when($request->date_to, function ($q, $dateTo){
+            $q->where('date', '<='. $dateTo);
+        });
+
+        $query->when($request->account_id, function ($q, $accountId){
+            $q->where('account_id', $accountId);
+        });
+
+        $query->when($request->category_id, function ($q, $categoryId){
+            $q->where('category_id', $categoryId);
+        });
+
+        $query->when($request->type, function ($q, $type){
+            $q->whereHas('category', function ($subQuery) use ($type){
+                $subQuery->where('type', $type);
+            });
+        });
+
+        $sortBy = $request->input('sort_by', 'date');
+        $sortDir = $request->input('sort_dir', 'desc');
+
+
+        return $query->orderBy($sortBy, $sortDir)
+            ->paginate(15);
     }
 
     /**
