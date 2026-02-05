6a548f12a536335cabe30a30a9d0e532a9fb8eac
vohasal <kulyashovr@gmail.com>
2026-02-05 14:01:47 +0500

    feat(models): add softDeletes

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,11 +8,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
 
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $fillable = [
         'name',
         'type',
